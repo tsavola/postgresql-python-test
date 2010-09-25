5a8608412e4fefb2cc53f710fe115a2cc0ae1282
Timo Savola <timo.savola@iki.fi>
2010-09-25 19:35:01 +0300

    even better test

--- pgpt/test.py
+++ pgpt/test.py
@@ -2,19 +2,18 @@ from . import database
 from . import store
 
 def main():
-	s = store.make_store()
-	name = local(s)
-	value = remote(s.name)
-	print(value)
+	storename = local()
+	remote(storename)
 
-def local(s):
+def local():
+	s = store.make_store()
 	s["key1"] = "value1"
 	s["key2"] = "value2"
 	del s["key1"]
+	return s.name
 
 @database.call
-def remote(name):
-	s = store.get_store(name)
-	value = s["key2"]
+def remote(storename):
+	s = store.get_store(storename)
+	assert s["key2"] == "value2"
 	s.drop()
-	return value
